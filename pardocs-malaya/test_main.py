d7cd2d103e7990a7660ef2110124b8e254f31ef9
Michael Leow <leow@sinarproject.org>
2019-07-03 22:48:07 +0800

    Summary: Extract some test PDF data for use by Malaya

--- pardocs-malaya/test_main.py
+++ pardocs-malaya/test_main.py
@@ -1,7 +1,9 @@
+from pprint import pprint
+
 import simplejson as json
 from unittest import TestCase
 from main import hello_world
-
+from PyPDF2 import PdfFileReader
 
 class TestHelloWorld(TestCase):
     def test_hello_world(self):
@@ -12,3 +14,24 @@ class TestHelloWorld(TestCase):
         data = json.loads(msg)
         print("DATA ==> " + data['message'])
         # self.fail()
+
+    def test_malaya(self):
+        pread = PdfFileReader(open("/Users/leow/GOMOD/go-pardocs/splitout/par14sesi1-soalan-BukanLisan-3.pdf",  "rb"))
+
+        print(pread.getNumPages())
+
+        pg1 = pread.getPage(0).extractText()
+        # pprint(' '.join(pg1.splitlines()).split())
+        pg2 = pread.getPage(1).extractText()
+        # pprint(pg2)
+        # questionpg = ''.join(pg1.splitlines()).split()
+        questionpg = ' '.join(''.join(pg1.splitlines()).split())
+        questionpg += ' '.join(''.join(pg2.splitlines()).split())
+
+        # finalpg = questionpg.splitlines()
+        # pprint(questionpg.split()[12])
+
+        pprint(questionpg)
+        print("\n\n\n")
+        print(questionpg)
+        # pprint(' '.join(finalpg))
