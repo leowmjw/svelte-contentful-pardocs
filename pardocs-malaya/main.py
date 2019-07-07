27fa867df3eb575d37c491a5e600eff2f1cc62a0
Michael Leow <leow@sinarproject.org>
2019-07-07 17:02:48 +0800

    Summary: Tested more malaya + added sveltjs sapper structure

--- pardocs-malaya/main.py
+++ pardocs-malaya/main.py
@@ -1,11 +1,14 @@
 """ First API; local only """
 import hug
 
+import mymalaya
+
 
 @hug.cli()
 @hug.get(examples='name=Bob&age=10')
 @hug.local()
 def hello_world(name: hug.types.text, age: hug.types.number, hug_timer=100):
     """Says goodbye world!!"""
-    return {'message': 'Goodbye {0} World!! {1}! Sib!!!'.format(age, name),
-            'took': float(hug_timer)}
+    mymalaya.test_preprocess()
+    return {"message": "Goodbye {0} World!! {1}! Sib!!!".format(age, name),
+            "took": float(hug_timer)}
--- pardocs-malaya/mymalaya.py
+++ pardocs-malaya/mymalaya.py
@@ -3,11 +3,22 @@ from pprint import pprint
 import malaya
 
 
+def test_preprocess():
+    content = "abc def hello"
+    string_4 = 'aahhh, malasnye nak pegi keje harini #mondayblues'
+    preprocessing = malaya.preprocessing.preprocessing()
+    pprint(preprocessing.process(string_4))
+    # pprint(malaya.preprocessing.get_normalize())
+    # tokenizer = malaya.preprocessing.SocialTokenizer().tokenize
+    # pprint(tokenizer(string_4))
+
+
 def main():
-    print("Hello Malaya!!  ")
+    print("Hello Malaya!!  " + malaya.home)
     malaya.print_cache()
-    corpus = ["bob", "dude"]
-    pprint(malaya.pos.available_deep_model())
+    # corpus = ["bob", "dude"]
+    # pprint(malaya.pos.available_deep_model())
+    test_preprocess()
 
 
 if __name__ == "__main__":
